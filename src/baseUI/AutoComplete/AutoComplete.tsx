ee387725135cc21371db3ab09df990f96e14879f
Michael <michaelkatom10@gmail.com>
2023-04-06 17:16:58 +0300

    refactor to autocomplet almost done

--- src/baseUI/AutoComplete/AutoComplete.tsx
+++ src/baseUI/AutoComplete/AutoComplete.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./AutoComplete.scss";
 
 type Props = {
@@ -8,73 +8,93 @@ type Props = {
   onSelection: React.Dispatch<React.SetStateAction<string | undefined>>;
 };
 
-export const AutoComplete: FC<Props> = ({ suggestions, onSelection, placeholder, selectedOption }) => {
-  const [active, setActive] = useState(0);
-  const [filtered, setFiltered] = useState([]);
-  const [isShow, setIsShow] = useState(false);
-  const [input, setInput] = useState("");
+export const AutoComplete: React.FC<Props> = ({ suggestions, onSelection, placeholder, selectedOption }) => {
+  const [active, setActive] = useState<number>(0);
+  const [filtered, setFiltered] = useState<string[]>([]);
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const [input, setInput] = useState<string>("");
+
+  const autoCompleteRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    setInput(selectedOption);
+    setInput(selectedOption ?? "");
   }, [selectedOption]);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const input = e.currentTarget.value;
-    const newFilteredSuggestions: any = suggestions.filter(
-      (suggestion) => suggestion.toLowerCase().indexOf(input.toLowerCase()) > -1
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (autoCompleteRef.current && !autoCompleteRef.current.contains(event.target as Node)) {
+      setIsShow(false);
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "ArrowUp") {
+      event.preventDefault();
+      setActive((prevActive) => (prevActive - 1 >= 0 ? prevActive - 1 : filtered.length - 1));
+    } else if (event.key === "ArrowDown") {
+      event.preventDefault();
+      setActive((prevActive) => (prevActive + 1 < filtered.length ? prevActive + 1 : 0));
+    } else if (event.key === "Escape") {
+      setIsShow(false);
+    } else if (event.key === "Enter") {
+      event.preventDefault();
+      setInput(filtered[active]);
+      onSelection(filtered[active]);
+      setIsShow(false);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.currentTarget.value;
+    const newFilteredSuggestions = suggestions.filter((suggestion) =>
+      suggestion.toLowerCase().includes(inputValue.toLowerCase())
     );
     setActive(0);
     setFiltered(newFilteredSuggestions);
+    setInput(inputValue);
     setIsShow(true);
-    setInput(e.currentTarget.value);
   };
-  const onClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
-    setActive(0);
-    setFiltered([]);
+
+  const handleSuggestionClick = (suggestion: string) => {
+    setInput(suggestion);
+    onSelection(suggestion);
     setIsShow(false);
-    setInput(e.currentTarget.innerText);
-    onSelection(e.currentTarget.innerText);
   };
-  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      setActive(0);
-      setIsShow(false);
-      setInput(filtered[active]);
-      onSelection(filtered[active]);
-    } else if (e.key === "ArrowUp") {
-      return active === 0 ? null : setActive(active - 1);
-    } else if (e.key === "ArrowDown") {
-      return active - 1 === filtered.length ? null : setActive(active + 1);
-    }
+
+  const handleOnFocus = () => {
+    setIsShow(true);
   };
-  const renderAutocomplete = () => {
-    if (isShow && input) {
-      if (filtered.length) {
-        return (
-          <ul className="autocomplete">
-            {filtered.map((suggestion, index) => {
-              return (
-                <li className={index === active ? "active" : ""} key={suggestion} onClick={onClick}>
-                  {suggestion}
-                </li>
-              );
-            })}
-          </ul>
-        );
-      }
-    }
-  };
+
   return (
-    <div className="autocompleteInput">
+    <div className="autocompleteInput" ref={autoCompleteRef}>
       <input
         type="text"
-        onFocus={() => setIsShow(true)}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        value={input ?? ""}
+        value={input}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        onFocus={handleOnFocus}
         placeholder={placeholder}
       />
-      {renderAutocomplete()}
+      {isShow && (
+        <ul className="autocomplete">
+          {filtered.map((suggestion, index) => (
+            <li
+              key={suggestion}
+              className={active === index ? "active" : ""}
+              onClick={() => handleSuggestionClick(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
